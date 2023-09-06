85e1629f4bff88e873fac971e590d3ed2cb58175
sanju sanjay <sanjay999bc@gamnil.com>
2023-09-06 14:31:03 +0530

    first

---
+++ MySpringDemo/src/main/java/com/jsp/springproject/MySpringDemo/MySpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.springproject.MySpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringDemoApplication.class, args);
+	}
+
+}
---
+++ MySpringDemo/src/test/java/com/jsp/springproject/MySpringDemo/MySpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.springproject.MySpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
